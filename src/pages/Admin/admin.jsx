b0ba9ef90483adbc32bb0fdfea3a9534bc8103de
ldf <1353391279@qq.com>
2021-04-10 23:19:30 +0800

    feat:admin页面开发

--- src/pages/Admin/admin.jsx
+++ src/pages/Admin/admin.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Layout } from 'antd';
 import './admin.css'
 
 export default function Admin(props) {
+  const { Header, Sider, Content } = Layout;
   //用户没有登录跳转到登录页
   if (!localStorage.getItem('token')) {
     return <Redirect to="/login" />
@@ -17,8 +18,18 @@ export default function Admin(props) {
 
   return (
     <div className="admin">
-      后台页面
-      <Button type="primary" onClick={logOut}>退出登录</Button>
+      <Layout style={{ height: 100 + '%' }}>
+        <Header>
+          <div className='header_title'>
+            <h1>电商后台管理系统</h1>
+          </div>
+          <Button type="primary" onClick={logOut}>退出登录</Button>
+        </Header>
+        <Layout>
+          <Sider>Sider</Sider>
+          <Content>Content</Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
